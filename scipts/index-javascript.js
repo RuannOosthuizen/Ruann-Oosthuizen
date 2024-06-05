95cca8a378ca65099a4a6d565de892cc74d2beee
Digital Asset Studios <ruann@operations.finance>
2024-06-05 12:19:23 +0200

    attempting to add side ways scroll (failed)

---
+++ scipts/index-javascript.js
@@ -0,0 +1,16 @@
+// script.js
+console.log("JavaScript file loaded");
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM fully loaded and parsed");
+    document.addEventListener('wheel', function(e) {
+        if (e.deltaY !== 0) {
+            e.preventDefault();
+            document.querySelector('.horizontal-scroll-content').scrollBy({
+                left: e.deltaY,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
